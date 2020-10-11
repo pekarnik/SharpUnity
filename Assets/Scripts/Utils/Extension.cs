1c3ef90cdf68173188cc817ca1564ca27918203c
Pekarnik <pekgoglo@gmail.com>
2020-10-12 01:47:17 +0300

    Task_3 Solved

--- Assets/Scripts/Utils/Extension.cs
+++ Assets/Scripts/Utils/Extension.cs
@@ -8,11 +8,39 @@ namespace Assets.Scripts.Utils
 {
 	public static class Extension
 	{
-		public static int Count<String>(this string self)
+		public static int Count(this string self)
 		{
 			// В задание именно найти количество символов в строке, думаю оно либо неправильно сформулировано 
 			// в методичке, либо я его не так понял
 			return self.Length;
+		}
+		public static Dictionary<T,int> ElCount0<T>(this List<T> list)
+		{
+			//a+b часть
+			Dictionary<T, int> res= new Dictionary<T, int>();
+			list.Sort();
+			int ind = 0;
+			res.Add(list[ind], 1);
+			for(var i = 1;i<list.Count;i++)
+			{
+				if(list[ind].Equals(list[i]))
+				{
+					res[list[ind]]++;
+				}
+				else
+				{
+					res.Add(list[i], 1);
+					ind = i;
+				}
+			}
+			//c часть
+			var uniqList = list.Distinct();
+			List<int> resList = new List<int>();
+			foreach(T t in uniqList)
+			{
+				resList.Add(list.Count(s => s.Equals(t)));
+			}
+			return res;
 		}
 	}
 }
