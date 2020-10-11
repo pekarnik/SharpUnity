88d511d3b9ff05ed0820431f8cc5c97c7151f71d
Pekarnik <pekgoglo@gmail.com>
2020-10-12 01:20:01 +0300

    Task_2 Solved

---
+++ Assets/Scripts/Utils/Extension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Utils
+{
+	public static class Extension
+	{
+		public static int Count<String>(this string self)
+		{
+			// В задание именно найти количество символов в строке, думаю оно либо неправильно сформулировано 
+			// в методичке, либо я его не так понял
+			return self.Length;
+		}
+	}
+}
