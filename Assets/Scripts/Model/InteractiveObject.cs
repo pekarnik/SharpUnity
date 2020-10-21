153743544589b76cee595432cc8271717b5bbda3
Pekarnik <pekgoglo@gmail.com>
2020-10-22 02:58:12 +0300

    Lesson 7

--- Assets/Scripts/Model/InteractiveObject.cs
+++ Assets/Scripts/Model/InteractiveObject.cs
@@ -8,6 +8,31 @@ namespace Assets.Scripts.Model
 	{
 		protected Color _color;
 		private bool _isInteractible;
+
+		[SerializeField] private bool _isAllowScaling;
+		[SerializeField] private float ActiveDis;
+
+		//……….
+
+		private void OnDrawGizmos()
+		{
+			Gizmos.DrawIcon(transform.position, "bot.jpg", _isAllowScaling);
+		}
+
+		private void OnDrawGizmosSelected()
+		{
+#if UNITY_EDITOR
+			Transform t = transform;
+
+			//Gizmos.matrix = Matrix4x4.TRS(t.position, t.rotation, t.localScale);
+			//Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+
+			var flat = new Vector3(ActiveDis, 0, ActiveDis);
+			Gizmos.matrix = Matrix4x4.TRS(t.position, t.rotation, flat);
+			Gizmos.DrawWireSphere(Vector3.zero, 5);
+#endif
+		}
+
 		protected bool IsInteractable
 		{
 			get { return _isInteractible; }
